83ee5cbc0a299f4662daabec64f2c4da8eb3be4a
hamed heydari <110024427+hamedhy79@users.noreply.github.com>
2025-07-27 22:45:56 +0330

    updated

--- ChatRoom/ChatRoom/settings.py
+++ ChatRoom/ChatRoom/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&tvv-vhmu6deskqbz!d@muc&8_t%z(js6#s9if3sqxma4z8i8)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
